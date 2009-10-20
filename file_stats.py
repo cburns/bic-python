8b0cc744cba4f67a0c2dce3dec1542e6a0f9d669
Christopher Burns <cburns@berkeley.edu>
2009-10-20 15:59:26 -0700

    Add code to walk directory tree.

--- file_stats.py
+++ file_stats.py
@@ -1,6 +1,8 @@
 
 import os
 import glob
+import fnmatch
+import locale
 
 def all_files(pattern, search_path, pathsep=os.pathsep):
     for path in search_path.split(pathsep):
@@ -13,6 +15,22 @@ def file_sizes(file_list):
     size_mean = size_sum / float(len(size_list))
     return size_sum, size_mean
 
+def all_dirs(root, patterns='*', single_level=False, yield_folders=False):
+    """Python Cookbook Recipe 2.16
+    patterns is a semicolon-separated string.
+    """
+    patterns = patterns.split(';')
+    for path, subdirs, files in os.walk(root):
+        if yield_folders:
+            files.extend(subdirs)
+        files.sort()
+        for name in files:
+            for pattern in patterns:
+                if fnmatch.fnmatch(name, pattern):
+                    yield os.path.join(path, name)
+                    break
+        if single_level:
+            break
 
 if __name__ == '__main__':
     pattern = '*.nii*'
@@ -28,4 +46,12 @@ if __name__ == '__main__':
     print 'total size:', asum
     print 'average size:', amean
 
-
+    newlist = list(all_dirs(search_path, patterns='*.nii*;*.img*'))
+    asum, amean = file_sizes(newlist)
+    #print 'total size:', asum
+    #print 'average size:', amean
+    
+    locale.setlocale(locale.LC_ALL, "")
+    print 'Total size of nifti and analyze images:', \
+        locale.format('%d', asum, True)
+    print 'Average size:', locale.format('%d', amean, True) 
