97c5359b7062e080d939a54ed7051f3c9df92a33
Christopher Burns <cburns@berkeley.edu>
2009-11-30 12:44:47 -0800

    Add preferences and colormap option.

--- slice_plot.py
+++ slice_plot.py
@@ -4,9 +4,12 @@ from enthought.chaco.api import (ArrayPlotData, Plot, gray, GridContainer,
                                  HPlotContainer)
 from enthought.enable.component_editor import ComponentEditor
 from enthought.traits.api import (HasTraits, Instance, DelegatesTo, 
-                                  on_trait_change, Enum, Array, Int, Str)
-from enthought.traits.ui.api import (Item, View, Menu, MenuBar, Action)
+                                  on_trait_change, Enum, Array, Int, Str,
+                                  Color, List, Trait, Callable, Dict)
+from enthought.traits.ui.api import (Item, View, Menu, MenuBar, Action,
+                                     OKButton, CancelButton)
 from enthought.chaco.tools.cursor_tool import CursorTool, BaseCursorTool
+import enthought.chaco.default_colormaps as chaco_colormaps
 from enthought.enable.api import BaseTool
 from enthought.chaco.tools.api import LineInspector
 
@@ -86,7 +89,8 @@ class SlicePlot(Plot):
         self.voxel = kwtraits.get('voxel') # XXX what to set for default?
 
     def set_slice(self, name):
-        self.renderer = self.img_plot(name, hide_grids=False)[0]
+        self.renderer = self.img_plot(name, hide_grids=False, 
+                                      colormap=chaco_colormaps.gray)[0]
         self.slicename = name
         self.init_cursor()
 
@@ -118,9 +122,74 @@ class SlicePlot(Plot):
         #print 'voxel:', self.voxel.get('x', 'y', 'z')
         #print 'intensity:', self.data[y,x]
 
+    def redraw(self):
+        """Redraw the plot."""
+        self.renderer.request_redraw()
+
+    def set_colormap(self, cmap):
+        """Set the colormap of the plot.
+
+        Parameters
+        ----------
+        cmap : str or chaco callable colormap
+            A colormap name or a colormap from
+            enthought.chaco.default_colormaps
+
+        """
+
+        clr_range = self.renderer.color_mapper.range
+        try:
+            # Try chaco colormap
+            color_mapper = cmap(clr_range)
+        except TypeError:
+            # If cmap is not callable (like if it's a string)
+            try:
+                # Try cmap as a string
+                cmap = chaco_colormaps.color_map_name_dict[cmap]
+                color_mapper = cmap(clr_range)
+            except KeyError:
+                # Not a string assume a chaco colormap function
+                msg = "Unable to find colormap '%s'" % cmap
+                raise KeyError(msg)
+        self.renderer.color_mapper = color_mapper
+        self.redraw()
+
+#
+# Menus and actions
+#
 file_open = Action(name = "Open...", action = "load_image")
 menu_file = Menu(file_open, name = "File")
-menubar = MenuBar(menu_file)
+preferences = Action(name = "Preferences", action = "set_preferences")
+menu_viewer = Menu(preferences, name = "Viewer")
+menubar = MenuBar(menu_viewer, menu_file)
+
+class Preferences(HasTraits):
+    colormap = Enum(chaco_colormaps.color_map_name_dict.keys())
+    cmap = Trait(chaco_colormaps.gray, Callable)
+
+    traits_view = View(Item('colormap'),
+                       title = "Preferences",
+                       buttons = [OKButton, CancelButton],
+                       resizable = True)
+
+    def __init__(self):
+        super(Preferences, self).__init__()
+
+    def _colormap_changed(self):
+        self.cmap = chaco_colormaps.color_map_name_dict[self.colormap]
+
+def launch_prefs(caller):
+    # XXX Need to think about better way to handle preferences.
+    prefs = Preferences()
+    prefs.edit_traits(kind='modal')
+    try:
+        # XXX just handling colormap for now.  Need to abstract
+        # preferences into an object that can be serialized and easily
+        # passed between objects (a dict?).
+        caller.update_preferences(prefs.cmap)
+    except AttributeError:
+        pass
+
 
 class Viewer(HasTraits):
     plot = Instance(GridContainer)
@@ -165,7 +234,6 @@ class Viewer(HasTraits):
 
     def update_slices(self):
         # Update image slices based on selecte voxel coords.
-        # XXX duplicate code
         axial = self.img.get_axial_slice(self.voxel.z)
         coronal = self.img.get_coronal_slice(self.voxel.y)
         sagittal = self.img.get_sagittal_slice(self.voxel.x)
@@ -199,7 +267,16 @@ class Viewer(HasTraits):
                 self.voxel.y = ydim/2
                 self.voxel.z = zdim/2
             self.update_slices()
-            
+
+    def set_preferences(self, info=None):
+        # XXX Separate Handler from view and attach actions to the
+        # handler.  This might allow me to have the preferenceces be
+        # separate View from the plot viewer. ???
+        launch_prefs(self)
+
+    def update_preferences(self, cmap):
+        for plot in self.container.components:
+            plot.set_colormap(cmap)
 
 if __name__ == '__main__':
     viewer = Viewer()
